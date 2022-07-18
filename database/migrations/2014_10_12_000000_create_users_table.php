06a44d7133b93a2553f3bd43ecf2f85d40286d19
Aleksey <alekseykarpelev@gmail.com>
2022-07-18 13:56:03 +1000

    add  mig fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('isadmin');
-            $table->string('avatar');
+            $table->boolean('isadmin')->nullable();
+            $table->string('avatar')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
